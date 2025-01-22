67af0ced6b745c5aae15084a00f2a652f02ca04a
christianjeandidier <133997310+christianjeandidier@users.noreply.github.com>
2025-01-22 22:54:40 +0000

    Delete chatapp_1/app/home/chat/[chatId] directory

--- chatapp_1/app/home/chat/[chatId]/_components/chat-header.jsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-export const SelectedChatHeader = ({ data }) => {
-    console.log(data, "CHAT HEADER");
-    return <div>ChatHeader</div>;
-};
--- chatapp_1/app/home/chat/[chatId]/page.jsx
+++
@@ -1,5 +0,0 @@
-const SelectedChatPage = () => {
-    return <div>This is selected chat page</div>;
-};
-
-export default SelectedChatPage;
